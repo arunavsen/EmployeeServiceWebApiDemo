c49f0ee66464ad5001503e4f5ac4e54efa897ed0
Arunav <arunav@vonome.com>
2021-03-07 13:49:48 +0600

    Add Employees to Database, Make local default database

---
+++ EmployeeServiceWebApiDemo/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EmployeeServiceWebApiDemo.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Employees> Employeeses { get; set; }
+
+        
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
