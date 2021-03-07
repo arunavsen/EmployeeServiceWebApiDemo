4aa5b494a29c984483291ab3633b4b5045699cc8
Arunav <arunav@vonome.com>
2021-03-07 13:55:06 +0600

    Add EmployeeController and Initialize it with ApplicationDbContext

---
+++ EmployeeServiceWebApiDemo/Controllers/EmplyoeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeServiceWebApiDemo.Models;
+
+namespace EmployeeServiceWebApiDemo.Controllers
+{
+    public class EmplyoeeController : ApiController
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmplyoeeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+    }
+}
