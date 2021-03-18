337e07cec908ac748225da61a8fa227f31d89b40
Arunav <arunav@vonome.com>
2021-03-18 17:53:15 +0600

    Attribute routing

---
+++ EmployeeServiceWebApiDemo/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices.ComTypes;
+using System.Web.Http;
+using EmployeeServiceWebApiDemo.Models;
+
+namespace EmployeeServiceWebApiDemo.Controllers
+{
+    public class StudentController : ApiController
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student(){Id = 1,Name = "Student 1"},
+            new Student(){Id = 2,Name = "Student 2"},
+            new Student(){Id = 3,Name = "Student 3"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(m => m.Id == id);
+        }
+
+        [Route("api/Student/GetStudentCourse/{id}")]
+        public IEnumerable<string> GetStudentCourse(int id)
+        {
+            if (id == 1)
+            {
+                return new List<string>(){"Asp.Net", "Html","CSS"};
+            }else if (id == 2)
+            {
+                return new List<string>() { "Node", "Express", "Mongo DB" };
+            }
+            else
+            {
+                return new List<string>() { "PHP", "Laravel", "CodeIgnitor" };
+            }
+        }
+    }
+}
---
+++ EmployeeServiceWebApiDemo/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeServiceWebApiDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
